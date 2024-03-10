0a3769d07b9857407f70942e084b7392588bff87
kaito01234 <43839796+kaito01234@users.noreply.github.com>
2024-03-10 21:25:16 +0900

    fix

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
-alert("Hello world!");
+window.onload = function () {
+  let bodyText = document.body.innerHTML;
+  bodyText = bodyText.replace(/Dashboard/g, "置き換え");
+  document.body.innerHTML = bodyText;
+};
